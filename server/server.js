878f903c7bf26e4687dd1615ca09c42015c4d411
Lightyaer <mmnyed@gmail.com>
2018-02-26 23:30:41 +0530

    port tweak

--- server/server.js
+++ server/server.js
@@ -2,16 +2,13 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//     res.send(index.html);
-// })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server Started on port ${port}`);
 })
 
