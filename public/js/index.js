cb2cccd2af0125e8d604781220d69579912beefb
Lightyaer <mmnyed@gmail.com>
2018-03-02 19:47:43 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,19 +2,12 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to Server');
-
-
-    socket.emit('createMessage', {
-        from: 'Jane',
-        text: 'Nothing much!'
-    })
 });
 
 socket.on('disconnect', function () {
     console.log('Disconnected from Server');
 });
 
-
-socket.on('newMessage', function (message) {
-    console.log(message);
-})
+socket.on('newMessage', function(message) {
+    console.log('newMessage',message);
+});
--- server/server.js
+++ server/server.js
@@ -14,19 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User Connected');
 
+
+    socket.on('createMessage', (message) => {
+        console.log(message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+
     socket.on('disconnect', () => {
         console.log('Disconnected from User')
-    });
-
-    socket.emit('newMessage', {
-        from: 'Mike',
-        text: 'Whats Up?'
-    });
-
-
-
-    socket.on('createMessage', (Message) => {
-        console.log(Message);
     });
 });
 
